8ca60720671cd9415822b07a6c83b20072ced93c
Emanuel Bitencourt <emanuelmagalhaes3m@gmail.com>
2023-08-13 22:40:55 -0300

    Atualizar arquivos compilados para implantação

--- src/components/apiData.js
+++
@@ -1,6 +0,0 @@
-import { Box, Text } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-
-export default function ApiData({ selectedCategory, value, url }) {
-  return <Box color="white"></Box>;
-}
